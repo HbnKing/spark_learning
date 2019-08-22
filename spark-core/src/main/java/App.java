834de09ee5dc3f9e956dfadf85ded04c15b96eaa
HbnKing <Hbn.King@gmail.com>
2019-08-22 17:40:50 +0800

    update with spark core

---
+++ spark-core/src/main/java/App.java
@@ -0,0 +1,8 @@
+import java.io.File;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(new File("resource").getAbsolutePath());
+    }
+}
